1aa049fa94214d47309e8276ef3659dbe0d6eb8f
diyorbek-sultanov <diyorbeksultonov1@mail.ru>
2023-06-08 16:49:32 +0500

    project-start

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+	title: 'Home | Instelloo',
+	description: 'This is home page in Instelloo web app'
+}
+
+export default function Page() {
+	return <div>Home</div>
+}
---
+++ tailwind.config.js
@@ -0,0 +1,68 @@
+/** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}'
+	],
+	theme: {
+		extend: {
+			colors: {
+				pink: '#DB2777',
+				textColor: '#333',
+				title: '#111827',
+				iconColor: '#707070',
+				sidebarIconColor: '#BE185D'
+			},
+			fontSize: {
+				sidebarFont: '15px',
+				textXs: '14px',
+				title: '24px'
+			},
+			transitionTimingFunction: {
+				DEFAULT: 'ease-in-out'
+			},
+			transitionDuration: {
+				DEFAULT: '210ms'
+			},
+			keyframes: {
+				sladeInUp: {
+					from: {
+						opacity: 0,
+						transform: 'translateY(100px)'
+					},
+					to: {
+						opacity: 1,
+						transform: 'translateY(0)'
+					}
+				}
+			},
+			animation: {
+				sladeInUp: 'sladeInUp .3s ease-in-out'
+			}
+		}
+	},
+	plugins: [
+		plugin(({ addUtilities, addComponents }) => {
+			addUtilities({
+				'.flex-center-between': {
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'space-between'
+				}
+			}),
+				addComponents({
+					'.btn-pink': {
+						backgroundColor: '#DB2777',
+						color: '#fff',
+						fonsSize: '14px',
+						borderRadius: '5px',
+						fontWeight: '600'
+					}
+				})
+		})
+	]
+}
